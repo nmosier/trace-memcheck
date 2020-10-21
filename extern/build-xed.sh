1d7223361b66ede98b34168a9db1a3e77319b2f1
Nicholas Hunter Mosier <nmosier@myth60.stanford.edu>
2020-10-21 10:12:39 -0700

    initial commit

---
+++ extern/build-xed.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if [ $# -ne 2 ]; then
+    echo "usage: $0 <xed-src-path> <xed-build-path>"
+    exit 1;
+fi
+
+SOURCE="$1"
+BUILD="$2"
+
+if ! [ -r "$BUILD/obj/libxed.a" -a -r "$BUILD/obj/wkit/include/xed/xed-interface.h" ]; then
+    cd "$BUILD"
+    "$SOURCE/mfile.py" --jobs=8 -s
+fi
---
+++ src/test.cc
@@ -0,0 +1,8 @@
+extern "C" {
+#include <xed/xed-interface.h>
+}
+
+int main(void) {
+xed_tables_init();
+return 0;
+}
