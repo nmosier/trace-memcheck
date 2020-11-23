d52802e29e6b7634e25de92521a60217fd571ddd
nmosier <nmosier@cs.stanford.edu>
2020-11-23 08:01:58 -0800

    initial POCs of RSB

---
+++ src/rsb-call.asm
@@ -0,0 +1,27 @@
+BITS 64
+
+SECTION .text
+
+EXTERN rsb_ptr
+EXTERN rsb_begin
+EXTERN rsb_end
+EXTERN dst
+	
+call_relbrd:
+	push qword [rel orig_ra]
+	push rax
+	mov rax, rsp
+	mov rsp, [rel rsb_ptr]
+	cmp rsp, [rel rsb_end]
+	je .after_entry
+	push qword [rel new_ra]
+	push qword [rel orig_ra]
+	mov [rel rsb_ptr], rsp
+.after_entry:
+	mov rsp, rax
+	pop rax
+	jmp dst
+
+orig_ra: dq 42
+new_ra:	 dq 42
+
---
+++ src/rsb-ret.asm
@@ -0,0 +1,23 @@
+BITS 64
+SECTION .text
+EXTERN rsb_ptr
+EXTERN rsb_begin
+EXTERN rsb_end
+
+ret_:
+	xchg rax, [rsp]
+	push rcx
+	mov rcx, [rel rsb_ptr]
+	cmp rcx, [rel rsb_begin]
+	je .mismatch
+	add qword [rel rsb_ptr], 16
+	cmp rax, [rcx]
+	jne .mismatch
+	mov rax, [rcx + 8]
+	pop rcx
+	xchg rax, [rsp]
+	ret
+.mismatch:
+	int3
+
+	
