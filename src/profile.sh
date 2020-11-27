c7bdfa83137f994baa1fbfbac98205f0c549f403
nmosier <nmosier@cs.stanford.edu>
2020-11-27 12:00:54 -0800

    fix profile script

--- src/profile.sh
+++ src/profile.sh
@@ -2,5 +2,7 @@
 
 CPUPROFILE_FREQUENCY=10000
 rm -f memcheck.txt memcheck.prof
-./memcheck -p -- "$@"
+if ! ./memcheck -p -- "$@"; then
+    exit 1
+fi
 pprof ./memcheck memcheck.prof | sort -grk5 > memcheck.txt
