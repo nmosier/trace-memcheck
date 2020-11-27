cc2e5936c6e7eeaf59b3f1a243433e3d7faa7c8f
nmosier <nmosier@cs.stanford.edu>
2020-11-27 15:50:00 -0800

    analyze conditional branches

---
+++ src/jcc.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+FILE=memcheck.jcc
+
+rm -f $FILE
+./memcheck -j -- "$@" 2>&1 | grep -E '^(JCC|FALLTHRU) ' | cut -d' ' -f1,2 | sort -k2 -k1 | uniq -c | awk "
+BEGIN {
+}
+
+/JCC/ {
+      jccs[\$3] = \$1;
+      if (!(\$3 in fallthrus)) {
+      	 fallthrus[\$3] = 0;
+      }
+}
+
+/FALLTHRU/ {
+      fallthrus[\$3] = \$1;
+      if (!(\$3 in jccs)) {
+      	 jccs[\$3] = 0;
+      }
+}
+
+END {
+    print \"CC\", \"JCC\", \"FALLTHRU\", \"FRAC\";
+    for (cc in jccs) {
+    	/* compute factor */
+	frac = jccs[cc] / fallthrus[cc];
+	if (frac == 0) {
+	   frac = \"inf\";
+	} else if (frac < 1) {
+	   frac = 1 / frac;
+	}
+    	print cc, jccs[cc], fallthrus[cc], frac;
+    }
+}
+" | column -t | tee $FILE
