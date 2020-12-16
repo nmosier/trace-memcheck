bbb6371cb1425b4d6d6f68f2960582eacce16b68
nmosier <nmosier@cs.stanford.edu>
2020-12-16 11:26:29 -0800

    initial commit

---
+++ src/settings.hh
@@ -0,0 +1,16 @@
+#pragma once
+
+constexpr bool FILL_SP_DEC           = false;
+constexpr bool FILL_SP_INC           = false;
+constexpr bool FILL_CALL             = false;
+constexpr bool TAINT_STACK           = false;
+constexpr bool CHANGE_PRE_STATE      = false;
+constexpr bool ABORT_ON_TAINT        = true;
+constexpr bool CALL_TRACKER          = true;
+constexpr bool JCC_TRACKER           = true;
+constexpr bool LOCK_TRACKER          = true;
+constexpr bool RDTSC_TRACKER         = true;
+constexpr bool BLOCK_SHARED_MAPS     = true;
+constexpr bool ASSERT_TAINT_ZERO     = true;
+constexpr bool JCC_RECORD_REGS       = true;
+constexpr bool CKSUM_REQUIRE_DATA_EQ = true;
