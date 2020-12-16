48863b54976f6f4970b40e75fe1046ae9d5a4acf
nmosier <nmosier@cs.stanford.edu>
2020-12-16 07:50:49 -0800

    xmm support

--- src/state.cc
+++ src/state.cc
@@ -136,3 +136,42 @@ bool State::is_zero() const {
   if (!snapshot().is_zero()) { return false; }
   return true;
 }
+
+unsigned long long user_regs_struct::*State::get_reg_ptr(xed_reg_enum_t xed_reg) {
+  switch (xed_reg) {
+  case XED_REG_RAX: return &user_regs_struct::rax;
+  case XED_REG_RBX: return &user_regs_struct::rbx;
+  case XED_REG_RCX: return &user_regs_struct::rcx;
+  case XED_REG_RDX: return &user_regs_struct::rdx;
+  case XED_REG_RDI: return &user_regs_struct::rdi;
+  case XED_REG_RSI: return &user_regs_struct::rsi;
+  case XED_REG_RSP: return &user_regs_struct::rsp;
+  case XED_REG_RBP: return &user_regs_struct::rbp;
+  case XED_REG_R8 : return &user_regs_struct::r8 ;
+  case XED_REG_R9 : return &user_regs_struct::r9 ;
+  case XED_REG_R10: return &user_regs_struct::r10;
+  case XED_REG_R11: return &user_regs_struct::r11;
+  case XED_REG_R12: return &user_regs_struct::r12;
+  case XED_REG_R13: return &user_regs_struct::r13;
+  case XED_REG_R14: return &user_regs_struct::r14;
+  case XED_REG_R15: return &user_regs_struct::r15;
+  default: abort();
+  }
+}
+
+unsigned long long& State::reg(xed_reg_enum_t xed_reg) {
+  return regs().*get_reg_ptr(xed_reg);
+}
+
+unsigned long long State::reg(xed_reg_enum_t xed_reg) const {
+  return regs().*get_reg_ptr(xed_reg);
+}
+
+std::ostream& State::xmm_print(std::ostream& os, unsigned idx) const {
+  auto xmm_it = xmm_end(idx);
+  while (xmm_it != xmm_begin(idx)) {
+    --xmm_it;
+    os << std::hex << (unsigned) *xmm_it;
+  }
+  return os;
+}
--- src/state.hh
+++ src/state.hh
@@ -3,19 +3,36 @@
 #include <sys/user.h>
 #include <string>
 #include <iostream>
+extern "C" {
+#include <xed/xed-interface.h>
+}
 
 #include "tracee.hh"
 #include "snapshot.hh"
 #include "config.hh"
 
+// TODO: Create Register and FPRegister classes. A lot of code for managing these is lumped in here.
 class State {
 public:
   State() {}
 
   const user_regs_struct& regs() const { return regs_; }
+  user_regs_struct& regs() { return regs_; }
   const user_fpregs_struct& fpregs() const { return fpregs_; }
+  user_fpregs_struct& fpregs() { return fpregs_; }
   Snapshot& snapshot() { return snapshot_; }
   const Snapshot& snapshot() const { return snapshot_; }
+
+  unsigned long long reg(xed_reg_enum_t xed_reg) const;
+  unsigned long long& reg(xed_reg_enum_t xed_reg);
+
+  uint8_t *xmm_begin(unsigned idx) { return (uint8_t *) fpregs().xmm_space + idx * 16; }
+  uint8_t *xmm_end(unsigned idx) { return (uint8_t *) fpregs().xmm_space + (idx + 1) * 16; }
+  const uint8_t *xmm_begin(unsigned idx) const { return (uint8_t *) fpregs().xmm_space + idx * 16; }
+  const uint8_t *xmm_end(unsigned idx) const {
+    return (uint8_t *) fpregs().xmm_space + (idx + 1) * 16;
+  }
+  std::ostream& xmm_print(std::ostream& os, unsigned idx) const;
 
   template <typename InputIt>
   void save(Tracee& tracee, InputIt begin, InputIt end) {
@@ -63,11 +80,13 @@ private:
   reg_t *regs_begin() { return reinterpret_cast<reg_t *>(&regs_); }
   const reg_t *regs_end() const { return reinterpret_cast<const reg_t *>(&regs_ + 1); }
   reg_t *regs_end() { return reinterpret_cast<reg_t *>(&regs_ + 1); }
-
+  
   const fpreg_t *fpregs_begin() const { return reinterpret_cast<const fpreg_t *>(&fpregs_); }
   fpreg_t *fpregs_begin() { return reinterpret_cast<fpreg_t *>(&fpregs_); }
   const fpreg_t *fpregs_end() const { return reinterpret_cast<const fpreg_t *>(&fpregs_ + 1); }
   fpreg_t *fpregs_end() { return reinterpret_cast<fpreg_t *>(&fpregs_ + 1); }
+
+  static unsigned long long user_regs_struct::*get_reg_ptr(xed_reg_enum_t xed_reg);
 };
 
 user_regs_struct operator^(const user_regs_struct& lhs, const user_regs_struct& rhs);
