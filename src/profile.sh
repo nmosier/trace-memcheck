aead186a4ff0a9e10080b06ef77cf92fa81afeed
nmosier <nmosier@cs.stanford.edu>
2020-12-19 18:57:53 -0800

    updated

--- src/profile.sh
+++ src/profile.sh
@@ -1,8 +1,34 @@
 #!/bin/bash
 
+usage() {
+    cat <<EOF
+usage: $0 [-h] [-o <file>] command [arg...]
+EOF
+}
+
+OUTF='memcheck.txt'
+
+while getopts "ho:" OPTCHAR; do
+    case $OPTCHAR in
+	'h')
+	    usage
+	    exit 0
+	    ;;
+	'o')
+	    OUTF="$OPTARG"
+	    ;;
+	*)
+	    usage >&2
+	    exit 1
+	    ;;
+    esac
+done
+
+shift $((OPTIND-1))
+
 CPUPROFILE_FREQUENCY=10000
 rm -f memcheck.txt memcheck.prof
 if ! ./memcheck -p -- "$@"; then
     exit 1
 fi
-pprof ./memcheck memcheck.prof > memcheck.txt # | sort -grk5 > memcheck.txt
+pprof ./memcheck memcheck.prof > "$OUTF" # | sort -grk5 > memcheck.txt
