2cdb91caeffa336273ac34bb38e590311de3d9f0
nmosier <nmosier@cs.stanford.edu>
2021-01-24 12:36:47 -0800

    implement malloc wrapper in memcheck-libc (does nothing)

--- src/memcheck/libc.c
+++ src/memcheck/libc.c
@@ -2,6 +2,10 @@
 #include <sys/syscall.h>
 #include <sys/time.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#define __USE_GNU
+#include <dlfcn.h>
 
 int clock_gettime(clockid_t clk_id, struct timespec *tp) {
   return (int) syscall(SYS_clock_gettime, clk_id, tp);
@@ -14,3 +18,24 @@ int gettimeofday(struct timeval *tv, struct timezone *tz) {
 time_t time(time_t *tloc) {
   return (time_t) syscall(SYS_time, tloc);
 }
+
+
+/* Heapcheck Module */
+
+/* malloc() implementation */
+
+typedef void *malloc_t(size_t);
+static void *malloc_init(size_t size);
+
+static malloc_t *malloc_ptr = malloc_init;
+
+static void *malloc_init(size_t size) {
+  malloc_ptr = (malloc_t *) dlsym(RTLD_NEXT, "malloc");
+  return malloc_ptr(size);
+}
+
+void *malloc(size_t size) {
+  return malloc_ptr(size);
+}
+
+
