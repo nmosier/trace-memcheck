4778159b3f8dddfbbe9b6294bc933fe173a08a5a
nmosier <nmosier@cs.stanford.edu>
2021-01-24 16:20:59 -0800

    implement calloc in terms of malloc; safeguard recursion

--- src/memcheck/libc.c
+++ src/memcheck/libc.c
@@ -6,6 +6,11 @@
 #include <stdio.h>
 #define __USE_GNU
 #include <dlfcn.h>
+#undef __USE_GNU
+#include <stdbool.h>
+#include <assert.h>
+#include <errno.h>
+#include <string.h>
 
 int clock_gettime(clockid_t clk_id, struct timespec *tp) {
   return (int) syscall(SYS_clock_gettime, clk_id, tp);
@@ -22,20 +27,80 @@ time_t time(time_t *tloc) {
 
 /* Heapcheck Module */
 
+#define HEAPCHECK_BUFLEN 0x1000
+static char heapcheck_buf[HEAPCHECK_BUFLEN] = {0};
+static size_t heapcheck_idx = 0;
+#define HEAPCHECK_PADDING 8
+
+static char *heapcheck_alloc(size_t size) {
+  char *ptr = &heapcheck_buf[heapcheck_idx];
+  heapcheck_idx += size;
+  assert(heapcheck_idx <= HEAPCHECK_BUFLEN);
+  return ptr;
+}
+
 /* malloc() implementation */
-
-typedef void *malloc_t(size_t);
 static void *malloc_init(size_t size);
-
-static malloc_t *malloc_ptr = malloc_init;
+static void *(*malloc_ptr)(size_t) = malloc_init;
+static bool malloc_active = false;
 
 static void *malloc_init(size_t size) {
-  malloc_ptr = (malloc_t *) dlsym(RTLD_NEXT, "malloc");
+  if (malloc_active) {
+    return heapcheck_alloc(size);
+  } else {
+    malloc_active = true;
+    malloc_ptr = dlsym(RTLD_NEXT, "malloc");
+    malloc_active = false;
+  }
   return malloc_ptr(size);
 }
 
 void *malloc(size_t size) {
-  return malloc_ptr(size);
+  return malloc_ptr(size + HEAPCHECK_PADDING);
 }
 
+/* calloc() implementation
+ * implemented using malloc() for simplicity
+ */
+void *calloc(size_t nmemb, size_t size) {
+  /* check if nmemb & size overflow */
+  const size_t bytes = nmemb * size;
+  if (bytes != 0 && bytes / size != nmemb) {
+    errno = ENOMEM;
+    return NULL;
+  } else {
+    void *ptr = malloc(bytes);
+    if (ptr != NULL) {
+      memset(ptr, 0, bytes);
+    }
+    return ptr;
+  }
+}
 
+/* free() implementation */
+static void free_init(void *);
+static void (*free_ptr)(void *) = free_init;
+
+static void free_init(void *ptr) {
+  free_ptr = dlsym(RTLD_NEXT, "free");
+  return free_ptr(ptr);
+}
+
+void free(void *ptr) {
+  if (!malloc_active) {
+    return free_ptr(ptr);
+  }
+}
+
+/* realloc() implementation */
+static void *realloc_init(void *, size_t);
+static void *(*realloc_ptr)(void *, size_t) = realloc_init;
+
+static void *realloc_init(void *ptr, size_t size) {
+  realloc_ptr = dlsym(RTLD_NEXT, "realloc");
+  return realloc_ptr(ptr, size);
+}
+
+void *realloc(void *ptr, size_t size) {
+  return realloc_ptr(ptr, size + HEAPCHECK_PADDING);
+}
